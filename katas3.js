9ad5182964249a4b4abfc4f62baaed2cb675703e
Bruna Almeida Nunes da Conceicao <brunamamamu@gmail.com>
2021-12-28 15:09:21 -0300

    alteraçoes finais

--- katas3.js
+++ katas3.js
@@ -165,34 +165,82 @@ function kata11() {
     // implemente o código do kata 11 aqui
     //11. Exibir todos os números pares contidos em sampleArray. _(244, 758, 450, …, 940, 472)_
         const output = [];
-       
-    
-     
-        output.push(sampleArray);
-        
+       for (let contador = 0;contador < sampleArray.length;contador++){
+           if (sampleArray[contador] % 2 === 0){
+               output.push(sampleArray[contador]);
+           }
+       }
     
     showResults(output, 'kata 11');
 }
 const buttonkata11 = document.getElementById('kata11');
 buttonkata11.addEventListener('click', kata11);
 
-}
-
 function kata12() {
     // implemente o código do kata 12 aqui
+    //12. Exibir todos os números ímpares contidos em sampleArray. _(469, 755, 245, …, 179, 535)_
+        const output = [];
+       for (let contador = 1;contador < sampleArray.length;contador++){
+           if (sampleArray[contador] % 2 === 1){
+               output.push(sampleArray[contador]);
+           }
+       }
+    
+    showResults(output, 'kata 12');
 }
+const buttonkata12 = document.getElementById('kata12');
+buttonkata12.addEventListener('click', kata12);
+
 
 function kata13() {
     // implemente o código do kata 13 aqui
+    //13. Exibir os números divisíveis por 8 em sampleArray: _(712, 456, …, 472)_
+        const output = [];
+    for (let contador = 0;contador < sampleArray.length; contador++){
+       if (sampleArray[contador] % 8 === 0){
+               output.push(sampleArray[contador]);
+
+        
+    }
+    showResults(output, 'kata 13');
 }
+}
+const buttonkata13 = document.getElementById('kata13');
+buttonkata13.addEventListener('click', kata13);
+
 
 function kata14() {
     // implemente o código do kata 14 aqui
+    //14. Exibir o quadrado de cada elemento de sampleArray. _(219961, 570025, …, 222784)_
+    const output = []; 
+    for (let contador = 0;contador < sampleArray.length; contador++){
+        if(sampleArray[contador **= contador]){
+            
+            output.push(sampleArray[contador]);
+        }
+        showResults(output, 'kata14');
+
+    }
 }
+const buttonkata14 = document.getElementById('kata14');
+buttonkata14.addEventListener('click', kata14);
 
 function kata15() {
     // implemente o código do kata 15 aqui
+        //15. Exibir a soma de todos os números de 1 a 20.
+        const output = [];
+        const array = [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    for (let contador = 0;contador < array.length; contador++){
+      if(contador += contador ){
+
+        output.push(array[contador]);
+      }
+    }
+    showResults(output, 'kata 15');
 }
+const buttonkata15 = document.getElementById('kata15');
+buttonkata15.addEventListener('click', kata15);
+
 
 function kata16() {
     // implemente o código do kata 16 aqui
@@ -200,6 +248,9 @@ function kata16() {
 
 function kata17() {
     // implemente o código do kata 17 aqui
+
+
+
 }
 
 function kata18() {
